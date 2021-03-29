886264d2ef4e1b2471c60a05c393b1412faacc67
idinae <iddinae@gmail.com>
2021-03-29 16:15:08 +0300

    db connection

--- recipes/src/components/Item/Item.js
+++ recipes/src/components/Item/Item.js
@@ -1,35 +1,23 @@
 import style from './Item.module.css';
 
-const Item = () => {
+const Item = ({
+  id,
+  type,
+  name,
+  description,
+  imageUrl,
+}) => {
     return (
 
-<div className={style.row}>
+<div className={style.reciperow}>
 
     <div className={style.thumbnail}>
-      <a href="/details/cook/:id"><img className="image" src="./assets/images/img01.jpg" alt="" /></a>
-      <h3>name</h3>
-      <p>description
+      <a href="/details/cook/:id"><img className="image" src={imageUrl} alt="" /></a>
+      <h3>{name}</h3>
+      <p>{description}
       </p>
       <a href="/details/{{cook.id}}" className={style.button} onClick="onSelected(cook)">Details</a>
     </div>
-
-    <div className={style.thumbnail}>
-    <a href="/details/cook/:id"><img className="image" src="./assets/images/img02.jpg" alt="" /></a>
-    <h3>name</h3>
-    <p>description
-    </p>
-    <a href="/details/{{cook.id}}" className={style.button} onClick="onSelected(cook)">Details</a>
-    </div>
-
-    <div className={style.thumbnail}>
-    <a href="/details/cook/:id"><img className="image" src="./assets/images/img03.jpg" alt="" /></a>
-    <h3>name</h3>
-    <p>description
-    </p>
-    <a href="/details/{{cook.id}}" className={style.button} onClick="onSelected(cook)">Details</a>
-    </div>
-
-
 
 </div>
 
--- recipes/src/components/Main/Main.js
+++ recipes/src/components/Main/Main.js
@@ -1,25 +1,58 @@
+import { Component } from 'react';
+
 import style from './Main.module.css';
 import Item from '../Item';
 
-const Main = () => {
-    return (
-        <div>
-        <div className={style.banner} className={style.container}> 
-            <img src="./assets/images/img1.jpg" alt="banner-image" />
-        </div>
-        <div className={style.container}>
-        <header>
-            <h2>Топ 3 рецепти</h2>
-        </header>
+class Main extends Component {
+    constructor(props) {
+        super(props);
 
-        <div className={style.row}>
+        this.state = {
+            recipes: []
+        }
+    }
 
-            <Item />
+    //fetch all data from db
+    componentDidMount() {
 
-        </div>
-        </div>
-        </div>
-    );
+            fetch('http://localhost:5000/recipes')
+                .then(res => res.json())
+                .then(res => this.setState({recipes: res}))
+                .catch(error => console.log(error));
+    }
+
+
+    render() {
+        return (
+            <div>
+            <div className={style.banner} className={style.container}> 
+                <img src="./assets/images/img1.jpg" alt="banner-image" />
+            </div>
+            <div className={style.container}>
+            <header>
+                <h2>Всички рецепти</h2>
+            </header>
+
+            <div className={style.reciperow}>
+
+                {this.state.recipes.map(x => 
+                <Item 
+                    key={x.id} 
+                    id={x.id}
+                    type={x.type}
+                    name={x.name} 
+                    description={x.description} 
+                    imageUrl={x.imageUrl} 
+                    likes={x.likes} 
+                />)}
+
+
+
+            </div>
+            </div>
+            </div>
+        );
+    }
 }
 
 export default Main;
