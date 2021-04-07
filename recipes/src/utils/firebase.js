874014e86f6342165ac132b640b3b3b684fe0d6e
idinae <iddinae@gmail.com>
2021-04-07 21:11:45 +0300

    firebase app test

--- recipes/src/utils/firebase.js
+++ recipes/src/utils/firebase.js
@@ -2,12 +2,13 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCc8vgO-JUGazXJWnogxzGvYGe4yZCc1kk",
-    authDomain: "my-app-ba547.firebaseapp.com",
-    projectId: "my-app-ba547",
-    storageBucket: "my-app-ba547.appspot.com",
-    messagingSenderId: "432625977150",
-    appId: "1:432625977150:web:20536cc9a71cf7c19f0e6f"
+    apiKey: "AIzaSyAWqesyOqH2l0dDSUdZhqmdft3ZKsb6FAI",
+    authDomain: "recipes-666.firebaseapp.com",
+    projectId: "recipes-666",
+    storageBucket: "recipes-666.appspot.com",
+    messagingSenderId: "1048274752952",
+    appId: "1:1048274752952:web:3f4a38210c69cdcde06b4d",
+    measurementId: "G-C5LREM2C6X"
   };
 
   //check if not initialized yet
