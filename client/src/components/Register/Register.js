31981e06e497ad767374d4b8daed9ede094e8ad0
idinae <iddinae@gmail.com>
2021-12-28 02:02:16 +0200

    controlled form added in component Register

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import firebase from '../../utils/firebase';
 import style from './Register.module.css';
 
 const Register = ({
     history
 }) => {
+    const [username, setUsername] = useState('@');
+    const [password, setPassword] = useState('123456');
+    const [repeatPassword, setRepeatPassword] = useState('');
+
+    const onUsernameChangeHandler = (e) => {
+        setUsername(e.target.value)
+    };
+
+    const onPasswordChangeHandler = (e) => {
+        setPassword(e.target.value)
+    };
+
+    const onRepeatPasswordChangeHandler = (e) => {
+        setRepeatPassword(e.target.value)
+    };
+
     const onRegisterSubmitHanlder = (e) => {
         e.preventDefault();
 
@@ -23,11 +40,14 @@ const Register = ({
             <h1>Регистрирай се</h1>
             <form onSubmit={onRegisterSubmitHanlder}>
                 <label htmlFor="username">Имейл:</label>
-                <input type="text" name="username" id="username" />
+                <input type="text" name={username} id="username" onChange={onUsernameChangeHandler} />
+                <div className={`${style.validation} ${!username.includes('@') && style.show}`}>Please enter a valid email address.</div>
                 <label htmlFor="password">Парола:</label>
-                <input type="password" name="password" id="password" />
+                <input type="password" name={password} id="password" onChange={onPasswordChangeHandler} />
+                <div className={`${style.validation} ${password.length<6 && style.show}`}>Password length should be minimum 6 characters.</div>
                 <label htmlFor="repeatPassword">Повтори паролата:</label>
-                <input type="password" name="repeatPassword" id="repeatPassword"></input>
+                <input type="password" name={repeatPassword} id="repeatPassword" onChange={onRepeatPasswordChangeHandler}></input>
+                <div className={`${style.validation} ${password !== repeatPassword && style.show}`}>Passwords do not match.</div>
                 <button type="submit" value="Register" className={style.buttonstyle}>Изпрати</button>
             </form>
         </div>
